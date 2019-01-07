fb53c7f8a7487497764815ade29ec67bc8713e7e
Juuggo <Juuggo.c@gmail.com>
2019-01-07 17:43:29 +0800

    Correct the button behavior.

--- CSS_deer/deer.js
+++ CSS_deer/deer.js
@@ -1,10 +1,26 @@
 document.addEventListener('DOMContentLoaded', function(event) {
-    document.getElementById('walk').onclick = function(){
+    document.getElementById('walk').onclick = function walkanime(){
+        if(typeof timeoutID == "number") {
+            window.clearTimeout(timeoutID);
+            delete timeoutID;
+            console.log("deleted timeout");
+        }
+        console.log('clicked');
         var hands = document.getElementById('hands');
         var foots = document.getElementById('foots');
-        hands.childNodes[0].classList.toggle('walk_lh');
-        hands.childNodes[1].classList.toggle('walk_rh');
-        foots.childNodes[0].classList.toggle('walk_lf');
-        foots.childNodes[1].classList.toggle('walk_rf');
+        timeoutID = setTimeout(function() {
+            if(hands.childNodes[0].classList.contains('walk_lh')) {
+                hands.childNodes[0].classList.remove('walk_lh');
+                hands.childNodes[1].classList.remove('walk_rh');
+                foots.childNodes[0].classList.remove('walk_lf');
+                foots.childNodes[1].classList.remove('walk_rf');
+                console.log("removed classes.");
+            }
+        },2000);
+        hands.childNodes[0].classList.add('walk_lh');
+        hands.childNodes[1].classList.add('walk_rh');
+        console.log('added classes.');
+        foots.childNodes[0].classList.add('walk_lf');
+        foots.childNodes[1].classList.add('walk_rf');
     }
 });
